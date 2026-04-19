8319dc97abe344355d2d51a2a7c18d655f76cf5d
bautibustos <bautibustos10@gmail.com>
2026-04-18 21:28:28 -0300

    feat: se añadie la carga de productos, falta acer el listado ya que en este momento la funcion devuelve none

--- backend/api/routes/content/products.py
+++ backend/api/routes/content/products.py
@@ -1,4 +1,44 @@
 from fastapi import APIRouter, HTTPException, Response
-from backend.api.schemas.product import Product
+#from backend.api.schemas.product import Product
+import os, json
 
-router = APIRouter()
+#router = APIRouter()
+
+class Producto():
+    nombre_producto: str
+    nombre_imagen: str
+    precio: float 
+    descripcion: str
+
+    def nuevo(self, nombre_producto, nombre_imagen, precio, descripcion):
+        with open("product_list.json", "r") as file:
+            datos_old = json.load(file)
+        # leo cual es el ultimo el item del diccionario lo paso a entero y le sumo uno    
+        ultimo_item=str(int(list(datos_old)[-1])+1)
+        
+        # genero y cargo el nuevo item
+        datos_old[ultimo_item]={
+            "nombre_producto":nombre_producto,
+            "nombre_imagen":nombre_imagen,
+            "precio":precio,
+            "descripcion": descripcion   
+        }
+        # actualizo el archivo
+        with open("product_list.json", "w") as file:
+            json.dump(datos_old, file, indent=4)
+    
+    def listados():
+        with open("product_list.json", "r") as file:
+            datos_old = json.load(file)
+
+if __name__ == "__main__":
+    p = Producto() 
+    
+    p.nuevo(
+        nombre_producto="costilla",
+        nombre_imagen="imagennueva", 
+        precio=2.5, 
+        descripcion="esto es una descripcion"
+    )
+
+    print(Producto.listados())
