4945b05a7743b9bf84d72832b5d6df89e152dc87
bautibustos <bautibustos10@gmail.com>
2026-04-24 19:33:55 -0300

    update: actualizacion de json y estructura de los productos

--- backend/api/routes/content/products.py
+++ backend/api/routes/content/products.py
@@ -5,21 +5,29 @@ import os, json
 router = APIRouter()
 
 class Product():
-    slug_product: int    
+    id: int
+    slug_product: str
+    categoria: str    
     name_product: str
     img: str
     precio: float 
     description: str
 
 
-    def nuevo(self, name_product, img, precio, description):
+    def nuevo(self, name_product, img, precio, description, categoria):
         with open("product_list.json", "r") as file:
             datos_old = json.load(file)
         # leo cual es el ultimo el item del diccionario lo paso a entero y le sumo uno    
-        ultimo_item=str(int(list(datos_old)[-1])+1)
+        ultimo_item=str(int(list(datos_old)[-1])+1)# modificar por slug
+        # replace(" ", "_") 
         
+        
+        slug = name_product.replace(" ","-")
+        slug = slug.lower()
         # genero y cargo el nuevo item
         datos_old[ultimo_item]={
+            "slug_product":slug,
+            "categoria": categoria,
             "name_product":name_product,
             "img":img,
             "precio":precio,
@@ -31,7 +39,8 @@ class Product():
     
     @staticmethod 
     def listados():
-        with open("backend\\api\\routes\\content\\product_list.json", "r") as file:
+        print(os.getcwd())
+        with open("api\\routes\\content\\product_list.json", "r") as file:
             listado = json.load(file)
         return JSONResponse(status_code=200, content=listado)
 
@@ -44,7 +53,8 @@ if __name__ == "__main__":
     p = Product() 
     
     p.nuevo(
-        name_product="costilla",
+        name_product="costilla de vaca",
+        categoria="ropa",
         img="imagennueva", 
         precio=2.5, 
         description="esto es una description"
